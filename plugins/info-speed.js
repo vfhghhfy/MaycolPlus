6bd5088f8584307195e896bdb4f46e814e4bea4c
Ado <adonaylester85@gmail.com>
2025-06-04 18:32:41 -0600

    Update info-speed.js

--- plugins/info-speed.js
+++ plugins/info-speed.js
@@ -19,20 +19,20 @@ let muptime = clockString(_muptime)
 let chats = Object.entries(conn.chats).filter(([id, data]) => id && data.isChats)
 let groups = Object.entries(conn.chats).filter(([jid, chat]) => jid.endsWith('@g.us') && chat.isChats && !chat.metadata?.read_only && !chat.metadata?.announce).map(v => v[0])
 
-let texto = `${emoji} *${packname}*
-🚀 *Velocidad:*
-→ ${latensi.toFixed(4)}
 
-🕒 *Activo Durante:*
-→ ${muptime}
+let texto = `> ${emoji} *${packname}*
+> ❇ *Velocidad Actual:*
+> → ${latensi.toFixed(4)}
 
-💫 *Chats:*
-→ ${chats.length} *Chats privados*
-→ ${groups.length} *Grupos*
+> ✤ *Activa Durante:*
+> → ${muptime}
 
-🏆 *Servidor:*
-➤ *Ram ⪼* ${format(totalmem() - freemem())} / ${format(totalmem())}
-`
+> ✧ *Chats Totales:*
+> → ${chats.length} *Chats privados*
+> → ${groups.length} *Grupos*
+
+> ✦ *Server:*
+> ➤ *Ram ⪼* ${format(totalmem() - freemem())} / ${format(totalmem())}`.trim()
 
 m.react('✈️')
 
@@ -50,4 +50,4 @@ function clockString(ms) {
 let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
 let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
 let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
+return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')}
