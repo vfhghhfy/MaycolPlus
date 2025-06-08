3aac6122ec2916bbc69a51468f60f4e6a5a4d5c8
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-06-08 16:56:44 -0500

    Create _testbuttom.js

---
+++ plugins/_testbuttom.js
@@ -0,0 +1,42 @@
+const handler = async (m, { conn }) => {
+  const texto = `✨ *TEST DE BOTONES FLOTANTES* ✨
+
+¡Hola, soy MaycolBot! ⊂(｡•́‿•̀｡)つ
+
+Aquí te dejo unos botones para jugar:`;
+
+  const botones = [
+    {
+      buttonId: '.menu',
+      buttonText: { displayText: '📜 Menú Principal' },
+      type: 1
+    },
+    {
+      buttonId: '.minijuego',
+      buttonText: { displayText: '🎮 Jugar Minijuego' },
+      type: 1
+    },
+    {
+      buttonId: '.info',
+      buttonText: { displayText: 'ℹ️ Info del Bot' },
+      type: 1
+    }
+  ];
+
+  const mensaje = {
+    text: texto,
+    footer: 'By MaycolAI 🤖❤️',
+    buttons: botones,
+    headerType: 1
+  };
+
+  await conn.sendMessage(m.chat, mensaje, { quoted: m });
+};
+
+handler.help = ['testboton'];
+handler.tags = ['test', 'fun'];
+handler.command = ['testboton'];
+handler.register = true;
+handler.channel = true;
+
+export default handler;
