1dcacc99b178eedd794bfacd12a80128b6ac517d
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-06-29 01:21:32 -0500

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -84,7 +84,7 @@ const replyMessage = message.length === 0
 ? `✖️ No hay *Sub-Bots* conectados con la Academia Kamome...`
 : message;
 
-const responseMessage = `✎ 𝐋𝐢𝐬𝐭𝐚 𝐃𝐞 𝐇𝐚𝐧𝐚𝐤𝐨-𝐁𝐨𝐭𝐬 ✎\n\nHola Jeje ^^ Estos son las personas que me tienen a mi jeje <3\n\n> Hecho por *_SoyMaycol <3_*\n\nSi Quieres puedes hacerte Hanako-Bot con #code o #qr jeje ^^\n\n~*Estas lindas personitas me tienen jeje <3 en cual en total son ${users.length || '0'}*~\n\n${replyMessage}`;
+const responseMessage = `✎ 𝐋𝐢𝐬𝐭𝐚 𝐃𝐞 𝐇𝐚𝐧𝐚𝐤𝐨-𝐁𝐨𝐭𝐬 ✎\n\nHola Jeje ^^ Estos son las personas que me tienen a mi jeje <3\n\n> Hecho por *_${global.apodo}_*\n\nSi Quieres puedes hacerte Hanako-Bot con #code o #qr jeje ^^\n\n~*Estas lindas personitas me tienen jeje <3 en cual en total son ${users.length || '0'}*~\n\n${replyMessage}`;
 await _envio.sendMessage(m.chat, {text: responseMessage, mentions: _envio.parseMention(responseMessage)}, {quoted: m})
 break
 }
