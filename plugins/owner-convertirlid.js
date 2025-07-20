5f09e6c5ba695c0f1da79bff1f717570417a9032
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-07-19 19:07:18 -0500

    Update owner-convertirlid.js

--- plugins/owner-convertirlid.js
+++ plugins/owner-convertirlid.js
@@ -1,21 +1,29 @@
-const handler = async (m, { conn, usedPrefix, command, text }) => {
-  let lid = text?.trim();
+const handler = async (m, { conn, text, usedPrefix, command }) => {
+  const lid = text?.trim();
   if (!lid || !/^\d+$/.test(lid)) {
-    return conn.reply(m.chat,
-      `╭─❍「 ✦ 𝘾𝙊𝙈𝘼𝙉𝘿𝙊: *convertirlid* ✦ 」\n│\n├─ 📌 Usa: ${usedPrefix}convertirlid 264944184516817\n╰─✦`, m);
+    return m.reply(`📌 Usa: ${usedPrefix + command} 11747138220075`);
   }
 
-  let jidCus = `${lid}@c.us`;
-  let jidS = `${lid}@s.whatsapp.net`;
+  try {
+    let data = await conn.onWhatsApp(lid + '@lid');
 
-  return conn.reply(m.chat,
-    `╭─❍「 ✦ 𝙍𝙀𝙎𝙐𝙇𝙏𝘼𝘿𝙊 ✦ 」\n│\n├─ 🔐 LID Original: *${lid}@lid*\n├─ 📲 Chat estándar: *${jidCus}*\n├─ 🛠️ JID interno: *${jidS}*\n╰─✦`, m);
+    if (data?.[0]?.jid) {
+      return m.reply(`╭─❍「 ✦ RESULTADO ✦ 」
+│
+├─ 🔐 LID: *${lid}@lid*
+├─ ✅ Número Real: *${data[0].jid.replace(/@.+/, '')}*
+╰─✦`);
+    } else {
+      return m.reply('❌ No se pudo encontrar el número real de ese LID');
+    }
+  } catch (e) {
+    console.error(e);
+    return m.reply('❌ Hubo un error al intentar resolver el LID');
+  }
 };
 
-// CONFIGURACIÓN
-handler.help = ['convertirlid'];
+handler.command = ['resolverlid']
+handler.help = ['resolverlid <lid>'];
 handler.tags = ['tools'];
-handler.command = ['convertirlid']
-handler.register = true;
 
 export default handler;
