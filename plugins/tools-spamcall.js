35bd97eb5d17bb2c7fbc816f62d45fc43a77e940
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-08-09 13:16:16 -0500

    Update tools-spamcall.js

--- plugins/tools-spamcall.js
+++ plugins/tools-spamcall.js
@@ -1,16 +1,19 @@
-let fetch = require('node-fetch')
-let handler = async (m, { conn, text, usedPrefix }) => {
-  if (!text) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx`
-  let nomor = text.replace(/[^0-9]/gi, '').slice(2)
-  if (!nomor.startsWith('8')) throw `Contoh Penggunaan\n${usedPrefix}spamcall 628xxxxxxxx`
-  m.reply('_*Tunggu permintaan anda sedang diproses.....*_')
-  let anu = await fetch(`https://id.jagreward.com/member/verify-mobile/${nomor}`).then(a => a.json())
-  let spcall = `*Nomor bot* : _${anu.phone_prefix}_\n\n_Bot berhasil menlpon anda!_`
-  conn.reply(m.chat, `${spcall}`.trim(), m)
-}
-handler.help = ['spamcall <nomor>']
-handler.tags = ['tools']
-handler.command = /^(spamcall)$/i
-handler.limit = true
-handler.premium = true
-module.exports = handler
+import fetch from 'node-fetch';
+
+const handler = async (m, { conn, text, usedPrefix }) => {
+  if (!text) throw `Uso correcto:\n${usedPrefix}spamcall 628xxxxxxxx`;
+  let numero = text.replace(/[^0-9]/gi, '').slice(2);
+  if (!numero.startsWith('8')) throw `Uso correcto:\n${usedPrefix}spamcall 628xxxxxxxx`;
+  m.reply('_*Por favor espera, tu solicitud está en proceso...*_');
+  let respuesta = await fetch(`https://id.jagreward.com/member/verify-mobile/${numero}`).then(res => res.json());
+  let mensaje = `*Número del bot*: _${respuesta.phone_prefix}_\n\n_¡El bot te ha llamado con éxito!_`;
+  conn.reply(m.chat, mensaje.trim(), m);
+};
+
+handler.help = ['spamcall <numero>'];
+handler.tags = ['tools'];
+handler.command = /^(spamcall)$/i;
+handler.limit = true;
+handler.premium = true;
+
+export default handler;
