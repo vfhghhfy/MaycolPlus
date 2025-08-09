cf9593a6d0ca1329627f6f7467277033b8547fc9
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-08-09 12:56:58 -0500

    Create ai-suno.js

---
+++ plugins/ai-suno.js
@@ -0,0 +1,28 @@
+/* Creditos a Ado (Wirk) */
+import fetch from 'node-fetch' 
+
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+  let text = args.join(" ")
+  if (!text) return m.reply(`🎧 Ingresa un prompt pa generar música\n*Ejemplo:* ${usedPrefix + command} un rap triste sobre gatos`)
+
+  try {
+    await m.react('🕓') 
+
+    const res = await fetch(`https://myapiadonix.vercel.app/api/AImusic?prompt=${encodeURIComponent(text)}`)
+    if (!res.ok) return m.reply("⚠️ No se pudo generar la música, intenta más tarde")
+
+    const arrayBuffer = await res.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+
+    await conn.sendFile(m.chat, buffer, 'AImusic-Adonix.mp3', `🎤 Aquí tu canción generada con IA\nPrompt: ${text}`, m)
+
+  } catch (e) {
+    console.error(e)
+    m.reply("❌ Ocurrió un error generando la canción.")
+  }
+}
+
+handler.help = ['iamusic']
+handler.tags = ['ia']
+handler.command = ['iamusic', 'suno', 'cancionia']
+export default handler
