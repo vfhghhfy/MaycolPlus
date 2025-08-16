1331904bad564b60fafb608a43668d46c5c1875f
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-08-15 22:44:03 -0500

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -1,7 +1,7 @@
 import { WAMessageStubType } from '@soymaycol/maybailyes'
 import fetch from 'node-fetch'
-const redes = 'GitHub: SoySapo6';
-const dev = 'SoyMaycol';
+const redes = `GitHub: ${global.github}`
+const dev = global.dev
 
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return !0;
