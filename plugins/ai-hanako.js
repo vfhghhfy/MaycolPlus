1fa41841fe61359a292f162e33a997b28b0d36ef
SoyMaycol 3 <karatekidamericatv@gmail.com>
2025-08-15 22:52:22 -0500

    Update ai-hanako.js

--- plugins/ai-hanako.js
+++ plugins/ai-hanako.js
@@ -1,7 +1,7 @@
 import Starlights from "@StarlightsTeam/Scraper"
 
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-  if (!text) return conn.reply(m.chat, '[ ✰ ] Qué le quieres decir a *Ai Hoshino*?.\n\n`» Ejemplo :`\n' + `> *${usedPrefix + command}* Holaa`, m, rcanal)
+  if (!text) return conn.reply(m.chat, '[ ✰ ] Qué le quieres decir a *Hanako*?.\n\n`» Ejemplo :`\n' + `> *${usedPrefix + command}* Holaa`, m, rcanal)
   
   try {
     let character_id = "7dNFCYh0JOVzRz3e9ISOM6fi6q5mQBwfSfWCUPDW_dU" //Consigue el ID de tu preferencia en https://spicychat.ai
