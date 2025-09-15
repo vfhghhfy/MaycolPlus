0527140a7307775fda4573a037a129fed47038e4
𝐒𝐨𝐲𝕄𝕒𝕪𝕔𝕠𝕝 ᵒᶠⁱᶜⁱᵃˡ <karatekidamericatv@gmail.com>
2025-09-15 17:47:41 -0500

    Update gacha-topwaifus.js

--- plugins/gacha-topwaifus.js
+++ plugins/gacha-topwaifus.js
@@ -1,6 +1,6 @@
 import { promises as fs } from 'fs';
 
-const charactersFilePath = './src/database/characters.json';
+const charactersFilePath = './database/characters.json';
 
 async function loadCharacters() {
     try {
