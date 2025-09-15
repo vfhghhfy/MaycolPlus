e67fefbf467f2c615281bca51afe0ecc7a271282
𝐒𝐨𝐲𝕄𝕒𝕪𝕔𝕠𝕝 ᵒᶠⁱᶜⁱᵃˡ <karatekidamericatv@gmail.com>
2025-09-15 17:13:53 -0500

    Update buscador-yt.js

--- plugins/buscador-yt.js
+++ plugins/buscador-yt.js
@@ -1,44 +1,47 @@
-// Codigo hecho por SoyMaycol <3
-import fetch from "node-fetch"
+// Codigo hecho por SoyMaycol <3  
+import fetch from "node-fetch"  
 
-let handler = async (m, { conn, text, command }) => {
-  if (!text) return m.reply(`✦ Ejemplo:\n.${command} Gato`)
+let handler = async (m, { conn, text, command }) => {  
+  if (!text) return m.reply(`✦ Ejemplo:\n.${command} Gato`)  
 
-  try {
-    let api = await fetch(`https://mayapi.ooguy.com/yts?query=${encodeURIComponent(text)}&apikey=may-35c2e70b`)
-    let res = await api.json()
+  try {  
+    let api = await fetch(`https://mayapi.ooguy.com/yts?query=${encodeURIComponent(text)}&apikey=may-35c2e70b`)  
+    let res = await api.json()  
 
-    if (!res.status || !res.result?.length) 
-      return m.reply("No encontré nada uwu (╥﹏╥)")
+    if (!res.status || !res.result?.length)   
+      return m.reply("No encontré nada uwu (╥﹏╥)")  
 
-    let vid = res.result[0] // primer resultado
+    // Tomar solo los 3 primeros resultados  
+    let vids = res.result.slice(0, 3)  
 
-    let msg = `
-╭─❍「 ✦ YT Search ✦ 」
-│
-├─ 🎬 *Título:* ${vid.title}
-├─ 👤 *Autor:* ${vid.autor}
-├─ ⏱ *Duración:* ${vid.duration}
-├─ 👀 *Vistas:* ${vid.views}
-├─ 📅 *Subido:* ${vid.uploaded}
-│
-├─ 🔗 *Enlace:* ${vid.url}
-╰──────────────────
-    `.trim()
+    for (let vid of vids) {  
+      let msg = `  
+╭─❍「 ✦ YT Search ✦ 」  
+│  
+├─ 🎬 *Título:* ${vid.title}  
+├─ 👤 *Autor:* ${vid.autor}  
+├─ ⏱ *Duración:* ${vid.duration}  
+├─ 👀 *Vistas:* ${vid.views}  
+├─ 📅 *Subido:* ${vid.uploaded}  
+│  
+├─ 🔗 *Enlace:* ${vid.url}  
+╰──────────────────  
+      `.trim()  
 
-    await conn.sendMessage(m.chat, { 
-      image: { url: vid.banner }, 
-      caption: msg 
-    }, { quoted: m })
+      await conn.sendMessage(m.chat, {   
+        image: { url: vid.banner },   
+        caption: msg   
+      }, { quoted: m })  
+    }  
 
-  } catch (e) {
-    console.error(e)
-    m.reply("Error al buscar (╯︵╰,)")
-  }
-}
+  } catch (e) {  
+    console.error(e)  
+    m.reply("Error al buscar (╯︵╰,)")  
+  }  
+}  
 
-handler.help = ["yts <texto>", "ytsearch <texto>"]
-handler.tags = ["search"]
-handler.command = /^yts|ytsearch$/i
+handler.help = ["yts <texto>", "ytsearch <texto>"]  
+handler.tags = ["search"]  
+handler.command = /^yts|ytsearch$/i  
 
 export default handler
