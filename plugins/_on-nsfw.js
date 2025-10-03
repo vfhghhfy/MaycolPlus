e3e247a4cb27d529d3f910f6ef8c854fcb62eaae
𝐒𝐨𝐲𝕄𝕒𝕪𝕔𝕠𝕝 ᵒᶠⁱᶜⁱᵃˡ <karatekidamericatv@gmail.com>
2025-10-03 09:13:32 -0500

    Update _on-nsfw.js

--- plugins/_on-nsfw.js
+++ plugins/_on-nsfw.js
@@ -2,34 +2,33 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   let chat = global.db.data.chats[m.chat]
   if (!args[0]) {
     return m.reply(`
-╭─❍「 ✦ Hanako ✦ 」❍─╮
-🌸 Estado actual del NSFW: *${chat.nsfw ? '🌙 Activado' : '🚪 Desactivado'}*
+╭─❍「 🐶 Perritos Memeros 🐾 」❍─╮
+🐕 Estado actual del NSFW: *${chat.nsfw ? '🐶 Activado' : '🚪 Desactivado'}*
 ┆ Usa:
 ┆ ${usedPrefix + command} on
 ┆ ${usedPrefix + command} off
-╰─❍──────────❍─╯
+╰─❍───────────────❍─╯
 `)
   }
 
   if (/on|enable|1/i.test(args[0])) {
     chat.nsfw = true
     m.reply(`
-╭─❍「 ✦ Hanako ✦ 」❍─╮
-👻 Los conjuros prohibidos
-del *NSFW* fueron abiertos...
-┆ Cuidado, humano curioso. 🌙
-╰─❍──────────❍─╯
+╭─❍「 🐕 Perritos Memeros 🐾 」❍─╮
+🎉 El club de perritos traviesos está abierto...
+┆ ¡Cuidado con los memes calientes! 🔥
+╰─❍───────────────❍─╯
 `)
   } else if (/off|disable|0/i.test(args[0])) {
     chat.nsfw = false
     m.reply(`
-╭─❍「 ✦ Hanako ✦ 」❍─╮
-🔮 El sello sagrado fue colocado.
-┆ El *NSFW* quedó sellado. 🚪
-╰─❍──────────❍─╯
+╭─❍「 🐶 Perritos Memeros 🐾 」❍─╮
+🛑 Los perritos se calmaron.
+┆ El club de NSFW está cerrado. 🚪
+╰─❍───────────────❍─╯
 `)
   } else {
-    m.reply(`Formato incorrecto UwU.
+    m.reply(`Formato incorrecto 🐾.
 Ejemplo:
 ${usedPrefix + command} on
 ${usedPrefix + command} off`)
